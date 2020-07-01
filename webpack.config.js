dd790b8c1486f84133b8a9bd5d6dd755de8b57d6
mk <mk@MacBook-Pro.local>
2020-07-01 23:30:41 +0900

    complete

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,24 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]',
+          limit: 10000
+        }
+      }
+    ]
   }
 }
