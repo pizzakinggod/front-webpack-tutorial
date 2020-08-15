7168b73ba6531975888616bcbf0255b479aea40d
pkg/mkpark <-@->
2020-08-15 17:55:49 +0900

    프리티어 깃 훅 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,15 @@
-// TODO: eslint를 구성해 보세요. 
+const {ESLint} = require('eslint');
+
+// TODO: eslint를 구성해 보세요.
+module.exports = {
+  parser: "babel-eslint",
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  extends: "eslint:recommended",
+  env: {
+    node: true,
+    browser:true
+  }
+}
